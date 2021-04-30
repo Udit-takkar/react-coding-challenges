ad3174fe98b2a1e789806f8f7ed220d2efc956a2
Udit-takkar <53316345+Udit-takkar@users.noreply.github.com>
2021-04-30 14:12:57 +0530

    solved this challenge

--- spootify/src/config.js
+++ spootify/src/config.js
@@ -1,8 +1,8 @@
 export default {
   api: {
-    baseUrl: 'https://api.spotify.com/v1',
-    authUrl: 'https://accounts.spotify.com/api/token',
-    clientId: '',
-    clientSecret: ''
-  }
-}
+    baseUrl: "https://api.spotify.com/v1",
+    authUrl: "https://accounts.spotify.com/api/token",
+    clientId: "0526e90473644943a8b925129c12b26f",
+    clientSecret: "13b5a3f4e7ba4777b2b741d2eb877207",
+  },
+};
--- spootify/src/routes/Discover/components/Discover.js
+++ spootify/src/routes/Discover/components/Discover.js
@@ -1,6 +1,11 @@
-import React, { Component } from 'react';
-import DiscoverBlock from './DiscoverBlock/components/DiscoverBlock';
-import '../styles/_discover.scss';
+import React, { Component } from "react";
+import DiscoverBlock from "./DiscoverBlock/components/DiscoverBlock";
+import "../styles/_discover.scss";
+import qs from "querystring";
+import Ouroboro from "@bit/joshk.react-spinners-css.ouroboro";
+
+const config = require("../../../config");
+const axios = require("axios");
 
 export default class Discover extends Component {
   constructor() {
@@ -9,18 +14,81 @@ export default class Discover extends Component {
     this.state = {
       newReleases: [],
       playlists: [],
-      categories: []
+      categories: [],
+      isLoading: true,
     };
   }
+  componentDidMount = async () => {
+    const newReleases = await this.makeRequest("new-releases", "albums");
+    const playlists = await this.makeRequest("featured-playlists", "playlists");
+    const categories = await this.makeRequest("categories", "categories");
+    this.setState({
+      newReleases,
+      playlists,
+      categories,
+      isLoading: false,
+    });
+  };
 
+  makeRequest = async (path, resourceType) => {
+    const {
+      data: { access_token: token },
+    } = await axios.post(
+      "https://accounts.spotify.com/api/token",
+      qs.stringify({ grant_type: "client_credentials" }),
+      {
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+          Authorization: `Basic ${btoa(
+            `${config.default.api.clientId}:${config.default.api.clientSecret}`
+          )}`,
+        },
+      }
+    );
+
+    const res = await axios.get(
+      `${config.default.api.baseUrl}/browse/${path}?locale=en_US`,
+      {
+        headers: { Authorization: `Bearer ${token}` },
+      }
+    );
+
+    return res.data[resourceType].items;
+  };
   render() {
     const { newReleases, playlists, categories } = this.state;
 
     return (
       <div className="discover">
-        <DiscoverBlock text="RELEASED THIS WEEK" id="released" data={newReleases} />
-        <DiscoverBlock text="FEATURED PLAYLISTS" id="featured" data={playlists} />
-        <DiscoverBlock text="BROWSE" id="browse" data={categories} imagesKey="icons" />
+        {this.state.isLoading ? (
+          <div
+            style={{
+              height: "250px",
+              display: "flex",
+            }}
+          >
+            <Ouroboro style={{ margin: "auto" }} color="#be97e8" />
+          </div>
+        ) : (
+          <>
+            <DiscoverBlock
+              text="RELEASED THIS WEEK"
+              id="released"
+              data={newReleases}
+            />
+            <DiscoverBlock
+              text="FEATURED PLAYLISTS"
+              id="featured"
+              data={playlists}
+            />
+            <DiscoverBlock
+              text="BROWSE"
+              id="browse"
+              data={categories}
+              imagesKey="icons"
+            />
+          </>
+        )}
       </div>
     );
   }
